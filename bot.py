c0366e011f7421d4730c8837956a7b63daa9b5b9
Kousthubhbhat <93434418+Kousthubhbhat@users.noreply.github.com>
2021-11-09 19:05:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,18 @@ from urllib.parse import urlparse
 import aiohttp
 from pyrogram import Client, filters
 import requests
-from bs4 import BeautifulSoup
 import re
+
+##########################################################################################################
+userDB = environ.get('userDB')
+userDB = userDB.replace(' ', '').replace('\n', '').split(',')
+
+apiDB = environ.get('apiDB)
+apiDB = apiDB.replace(' ', '').replace('\n', '').split(',')
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-PDISK_API_KEY = environ.get('PDISK_API_KEY')
 CHANNEL = environ.get('CHANNEL')
 bot = Client('pdisk bot',
              api_id=API_ID,
@@ -18,6 +23,78 @@ bot = Client('pdisk bot',
              bot_token=BOT_TOKEN,
              workers=50,
              sleep_threshold=0)
+
+async def saver(item_id, apikey):
+    res = False
+    raw_item_id = item_id:
+    res = await requests(f'http://linkapi.net/open/clone_item?api_key={apikey}&item_id={item_id}', timeout=7)
+    res = res.json()
+    print(res)
+    res = res['data']['item_id']   
+    status = res['status']
+    if status == 500:
+        if res['msg'] == 'please try later':
+            print('waiting')
+            await asyncio.sleep(1)
+            item_id = await saver(item_id, apikey)
+            return item_id
+        elif res['msg'] == f'cannot repost from yourself {raw_item_id}':
+            res = raw_item_id
+            return res
+        else:
+            print(res)
+            res = 'No_Content_Found'
+            return res
+    if status == -1:
+        if res['msg'] == 'please try later':
+            print('waiting')
+            await asyncio.sleep(1)
+            item_id = await saver(item_id, apikey)
+            return item_id
+        elif res['msg'] == f'cannot repost from yourself {raw_item_id}':
+            res = raw_item_id
+            return res
+        else:
+            print(res)
+            res = False
+            return res
+    elif status == 1:
+        item_id = res['data']['item_id']
+        return item_id
+    else:
+        print(res)
+    await asyncio.sleep(1)
+
+async def ziko_function(caption, user):
+    if user in userDB:
+        user = userDB.index(user)
+        apikey = apiDB[user][0]
+    else:
+        return False
+    urlDB = re.findall("(?P<url>https?://[^\s]+)", caption)
+    for i in range(len(urlDB)):
+        url = urlDB[i]
+        if re.search('t\.me|telegram', url):
+            caption = caption.replace(url,f'{apiDB[user][1]}')
+            continue
+        r = requests.get(url)
+        url = r.url
+        if re.search('yout', url):
+            continue
+        dottest = re.findall('\.', url)
+        if len(dottest) >= 3:
+            continue
+        if re.search('share-video\?', url):
+            titem_id = url.lower().split('=')[-1]
+        item_id = await saver(titem_id, apikey)
+        if item_id == titem_id:
+            caption = caption.replace(url, f'https://pdisks.com/share-video?videoid={item_id}')
+        elif item_id == 'No_Content_Found':
+            caption = caption.replace(url, '')
+        elif item_id == False:
+            return
+    caption = re.sub('@[^\s]+', apiDB[user][2], caption)
+    return caption
 
 
 @bot.on_message(filters.command('start') & filters.private)
@@ -30,120 +107,17 @@ async def start(bot, message):
 @bot.on_message(filters.text & filters.private)
 async def pdisk_uploader(bot, message):
     new_string = str(message.text)
-    try:
-        pdisk_link = await multi_pdisk_up(new_string)
-        await message.reply(f'{pdisk_link}', quote=True)
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
-
+    caption = await ziko_function(new_string,user = message.chat.id)
+    await message.reply(f'<b>{caption}</b>', quote=True)
 
 @bot.on_message(filters.photo & filters.private)
 async def pdisk_uploader(bot, message):
     new_string = str(message.caption)
-    try:
-        pdisk_link = await multi_pdisk_up(new_string)
-        if(len(pdisk_link) > 1020):
-            await message.reply(f'{pdisk_link}', quote=True)
-        else:
-            await bot.send_photo(message.chat.id, message.photo.file_id, caption=f'{pdisk_link}')
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
-
-
-async def get_ptitle(url):
-    html_text = requests.get(url).text
-    soup = BeautifulSoup(html_text, 'html.parser')
-    for title in soup.find_all('title'):
-        pass
-    title = list(title.get_text())
-    title = title[8:]
-    str = '@' + CHANNEL + ' '
-    for i in title:
-        str = str + i
-    lst = list(html_text.split(","))
-    c = 0
-    for i in lst:
-        if ("""videoid""" in i):
-            found = lst[c]
-            break
-        c += 1
-
-    # pdisk.net link
-    pdisk_video_id = list(found.split(":"))
-    video_id = pdisk_video_id[2]
-    video_id = list(video_id.split(","))
-    v_id = video_id[0]
-    v_len = len(v_id)
-    v_id = v_id[1:v_len - 2]
-
-    v_url = 'https://pdisks.com/share-video?videoid=' + v_id
-    res = [str, v_url]
-    return res
-
-
-async def pdisk_up(link):
-    if ('pdisk' in link or 'kuklink' in link or 'kofilink' in link or 'cofilink' in link or 'bit' in link or 'vdshort' in link or 'Pdisklink' in link or 'Pdisks' in link or 'droppx' in link or 'dplinks' in link or 'www.dropxer.net' in link or 'www.pdisk' in link or 'www.pdisks' in link or 'wslinker' in link or 'cdinks' in link):
-        res = await get_ptitle(link)
-        title_pdisk = res[0]
-        link = res[1]
-    else:
-        title_new = urlparse(link)
-        title_new = os.path.basename(title_new.path)
-        title_pdisk = '@' + CHANNEL +' '+ title_new
-    res = requests.get(
-        'http://linkapi.net/open/create_item?link_type=link&content_src=' + link + '&source=2000&api_key=' + PDISK_API_KEY + '&dir_id=0&title=' + title_pdisk + '&description=Join_' + CHANNEL + '_for_more_like_this')
-    data = res.json()
-    data = dict(data)
-    print(data)
-    v_id = data['data']['item_id']
-    v_url = 'https://pdisks.com/share-video?videoid=' + v_id
-    return (v_url)
-
-
-async def multi_pdisk_up(ml_string):
-    new_ml_string = list(map(str, ml_string.split(" ")))
-    new_ml_string = await remove_username(new_ml_string)
-    new_join_str = "".join(new_ml_string)
-
-    urls = re.findall(r'(https?://[^\s]+)', new_join_str)
-
-    nml_len = len(new_ml_string)
-    u_len = len(urls)
-    url_index = []
-    count = 0
-    for i in range(nml_len):
-        for j in range(u_len):
-            if (urls[j] in new_ml_string[i]):
-                url_index.append(count)
-        count += 1
-    new_urls = await new_pdisk_url(urls)
-    url_index = list(dict.fromkeys(url_index))
-    i = 0
-    for j in url_index:
-        new_ml_string[j] = new_ml_string[j].replace(urls[i], new_urls[i])
-        i += 1
-
-    new_string = " ".join(new_ml_string)
-    return await addFooter(new_string)
-
-
-async def new_pdisk_url(urls):
-    new_urls = []
-    for i in urls:
-        new_urls.append(await pdisk_up(i))
-    return new_urls
-
-
-async def remove_username(new_List):
-    for i in new_List:
-        if('@' in i or 't.me' in i or 'https://bit.ly/3m4gabB' in i or 'https://bit.ly/pdisk_tuts' in i or 'telegra.ph' in i):
-            new_List.remove(i)
-    return new_List
-
+    caption = ziko_function(new_string)
+    await bot.send_photo(message.chat.id, message.photo.file_id, caption=f'<b>{caption}</b>')
 
 async def addFooter(str):
     footer = """
-
 
 
 ⭐️JOIN CHANNEL ➡️ t.me/""" + CHANNEL
